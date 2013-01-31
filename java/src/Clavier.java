651b6d41fa49e73178522ec988df0a6fe2ecc9f1
joboc <jobo100@hotmail.com>
2013-01-31 01:42:44 +0100

    added 2nd octave and keys coloring

--- java/src/Clavier.java
+++ java/src/Clavier.java
@@ -1,23 +1,105 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JPanel;
 
 public class Clavier extends JPanel{
 	
-	static int interstice = 5;
+	private static int interstice = 5;
+	private static int tailleBlanche = 70;
+	private static int nbOctaves = 2;
+	private static int nbNotes = 12 * nbOctaves;
+	public static int taille = 7 * tailleBlanche * nbOctaves;
+	
+	private boolean[] notesActives = new boolean[nbNotes];
+	private int[] mappingBlanchesNotes = new int[7];
+	private int[] mappingNoiresNotes = new int[5];
+    private static final Map<String, Integer> mappingNomsNotes = new HashMap<String, Integer>();
 
-	public void paintComponent(Graphics g){
-		g.setColor(Color.gray);
-		int tailleTouche = this.getWidth() / 7;
-		g.setColor(Color.white);
-		for (int i = 0; i < 7; ++i){
-			g.fillRect(interstice/2 + i * tailleTouche, 0, tailleTouche - interstice, this.getHeight());
+	Clavier(){
+		reset();
+		
+		mappingBlanchesNotes[0] = 0;
+		mappingNoiresNotes[0]   = 1;
+		mappingBlanchesNotes[1] = 2;
+		mappingNoiresNotes[1]   = 3;
+		mappingBlanchesNotes[2] = 4;
+		mappingBlanchesNotes[3] = 5;
+		mappingNoiresNotes[2]   = 6;
+		mappingBlanchesNotes[4] = 7;
+		mappingNoiresNotes[3]   = 8;
+		mappingBlanchesNotes[5] = 9;
+		mappingNoiresNotes[4]   = 10;
+		mappingBlanchesNotes[6] = 11;
+		
+		mappingNomsNotes.put("Do", 0);
+		mappingNomsNotes.put("Do#", 1);
+		mappingNomsNotes.put("Reb", 1);
+		mappingNomsNotes.put("Re", 2);
+		mappingNomsNotes.put("Re#", 3);
+		mappingNomsNotes.put("Mib", 3);
+		mappingNomsNotes.put("Mi", 4);
+		mappingNomsNotes.put("Fa", 5);
+		mappingNomsNotes.put("Fa#", 6);
+		mappingNomsNotes.put("Solb", 6);
+		mappingNomsNotes.put("Sol", 7);
+		mappingNomsNotes.put("Sol#", 8);
+		mappingNomsNotes.put("Lab", 8);
+		mappingNomsNotes.put("La", 9);
+		mappingNomsNotes.put("La#", 10);
+		mappingNomsNotes.put("Sib", 10);
+		mappingNomsNotes.put("Si", 11);
+	}
+
+	public void reset(){
+		for (int i = 0; i < 12 * nbOctaves; ++i){
+			notesActives[i] = false;
 		}
-		g.setColor(Color.black);
-		for (int i = 0; i < 6; ++i){
-			if (i != 2)
-				g.fillRect(tailleTouche*3/4 + tailleTouche * i, 0, tailleTouche/2, this.getHeight()*5/8);
+	}
+	
+	public void activate(ArrayList<String> noms){
+		for (int i = 0; i < noms.size(); ++i){
+			notesActives[mappingNomsNotes.get(noms.get(i))] = true;
+		}
+		this.repaint();
+	}
+	
+	private enum COULEUR_TOUCHE{
+		BLANCHE,
+		NOIRE
+	}
+	
+	private int indexNote(int octave, int touche, COULEUR_TOUCHE couleur){
+		int note = 0;
+		if (couleur == COULEUR_TOUCHE.BLANCHE){
+			note = mappingBlanchesNotes[touche];
+		} else if (couleur == COULEUR_TOUCHE.NOIRE){
+			note = mappingNoiresNotes[touche];
+		}
+		return 12 * octave + note;
+	}
+	
+	public void paintComponent(Graphics g){
+		int tailleOctave = 7 * tailleBlanche;
+		for (int iOctave = 0; iOctave < 2; ++iOctave){
+			for (int i = 0; i < 7; ++i){
+				g.setColor(Color.WHITE);
+				if (notesActives[indexNote(iOctave, i, COULEUR_TOUCHE.BLANCHE)])
+					g.setColor(Color.RED);
+				g.fillRect(iOctave * tailleOctave + interstice/2 + i * tailleBlanche, 0, tailleBlanche - interstice, this.getHeight());
+			}
+			for (int i = 0; i < 6; ++i){
+				g.setColor(Color.BLACK);
+				if (i != 2){
+					if (notesActives[indexNote(iOctave, i<2 ? i : i-1, COULEUR_TOUCHE.NOIRE)])
+						g.setColor(Color.RED);
+					g.fillRect(iOctave * tailleOctave + tailleBlanche*3/4 + tailleBlanche * i, 0, tailleBlanche/2, this.getHeight()*5/8);
+				}
+			}
 		}
 	}
 }
--- java/src/MainWindow.java
+++ java/src/MainWindow.java
@@ -1,18 +1,51 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Point;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class MainWindow extends JFrame{
 
+	Clavier clavier;
+	
     MainWindow(){
-        this.setSize(new Dimension(500, 400));
+        this.setSize(new Dimension(Clavier.taille, 400));
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
         
         this.getContentPane().setLayout(new BorderLayout());
-        this.add(new Clavier(), BorderLayout.CENTER);
+        clavier = new Clavier();
+        this.getContentPane().add(clavier, BorderLayout.CENTER);
+
+        JPanel panneauHaut = new JPanel();
+        JTextField txtInput = new JTextField("");
+        txtInput.setPreferredSize(new Dimension(50, 30));
+        panneauHaut.add(txtInput);
+        this.getContentPane().add(panneauHaut, BorderLayout.NORTH);
+        panneauHaut.setSize(new Dimension(this.getWidth(), 60));
+        txtInput.setLocation(new Point(panneauHaut.getWidth()/2 - txtInput.getWidth()/2, panneauHaut.getHeight()/2 - txtInput.getHeight()/2));
+    
+        this.repaint();
+        
+        txtInput.addKeyListener(new KeyAdapter(){
+        	public void keyPressed(KeyEvent e){
+        		if (e.getKeyCode() == KeyEvent.VK_ENTER){
+        			clavier.reset();
+        			ArrayList<String> noms = new ArrayList<String>();
+        			noms.add("Do");
+        			noms.add("Mi");
+        			noms.add("Sol");
+        			noms.add("Si");
+        			clavier.activate(noms);
+        		}
+        	}
+        });
     }
 }
